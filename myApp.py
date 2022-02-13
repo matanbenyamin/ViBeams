ceb42f8d8a4e723b4469f779e9a46dd4a8d13da3
Matan Benyamin <matan.benyamin@cu-bx.com>
2022-02-13 20:50:44 +0200

    first

--- myApp.py
+++ myApp.py
@@ -6,6 +6,8 @@ from streamlit_plotly_events import plotly_events
 import numpy as np
 from PIL import Image
 
+st.set_page_config(layout="wide")
+
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
@@ -19,8 +21,16 @@ if uploaded_file is not None:
 
 
     fig = px.imshow(im,  color_continuous_scale='gray')
+    # fig.data[0].update(mode = 'marker')
+    # st.plotly_chart(fig)
+
+
     selected_points = plotly_events(fig)
+    selected_points
     a=selected_points[0]
+    fig.add_scatter(x=[a["x"]], y=[a["y"]], fillcolor='red')
+
+
 
     # streamlit run myApp.py
 
@@ -29,37 +39,59 @@ if uploaded_file is not None:
     # Display saved result if it exist
     if "prev_point" in app_state:
         prev_point = app_state["prev_point"]
+        fig.add_scatter(x=[prev_point[0]], y=[prev_point[1]],fillcolor = 'rgb(0,1,1)')
+        selected_points = plotly_events(fig)
+
+
+
+    if "state" in app_state:
+        curr_state = int(app_state["state"][0])
+        curr_state = -curr_state
+    else:
+        curr_state = 1
+
 
 
     # Perform an operation, and save its result
-    st.experimental_set_query_params(prev_point=[a["x"], a["y"]])  # Save value
+    st.experimental_set_query_params(prev_point=[a["x"], a["y"]], state = curr_state)  # Save value
+
+    if "prev_point" in app_state:
+        spectrum = im[a["y"]:a["y"]+5,a["x"]:a["x"]+5]
+        spectrum = spectrum.flatten()
 
 
-    spectrum = im[a["y"]:a["y"]+5,a["x"]:a["x"]+5]
-    spectrum = spectrum.flatten()
+        int(prev_point[1])
+
+        spectrum_p = im[int(prev_point[1]):int(prev_point[1]) + 5, int(prev_point[0]):int(prev_point[0]) + 5]
+        spectrum_p = spectrum_p.flatten()
+
+        freqs = [
+        910,919,930,639,949,
+        850,862,877,889,897,
+        786,801,812,825,840,
+        726,738,751,766,779,
+        656,667,685,697,710]
+        df = pd.DataFrame(index = freqs, data = [[spectrum]],columns=['current'])
+        df = df.sort_index()
+
+        df_p = pd.DataFrame(index=freqs, data=[[spectrum_p]], columns=['previous'])
+        df_p = df_p.sort_index()
+
+        df = df.join(df_p)
+
+        # plot
+        fig2 = px.line(df, title='Spectrum')
+        curr_state
+        if curr_state>0:
+            fig2.data[0].line.color = "red"
+            fig2.data[1].line.color = "blue"
+        else:
+            fig2.data[1].line.color = "red"
+            fig2.data[0].line.color = "blue"
+
+        # fig2.add_scatter(x = df_p.index, y = df_p['1'])
 
 
-    int(prev_point[1])
+        st.plotly_chart(fig2)
+        # streamlit run myApp.py
 
-    spectrum_p = im[int(prev_point[1]):int(prev_point[1]) + 5, int(prev_point[0]):int(prev_point[0]) + 5]
-    spectrum_p = spectrum_p.flatten()
-
-    freqs = [
-    910,919,930,639,949,
-    850,862,877,889,897,
-    786,801,812,825,840,
-    726,738,751,766,779,
-    656,667,685,697,710]
-    df = pd.DataFrame(index = freqs, data = [[spectrum]],columns=['current'])
-    df = df.sort_index()
-
-    df_p = pd.DataFrame(index=freqs, data=[[spectrum_p]], columns=['previous'])
-    df_p = df_p.sort_index()
-
-    df = df.join(df_p)
-    fig2 = px.line(df, title='Spectrum')
-
-    # fig2.add_scatter(x = df_p.index, y = df_p['1'])
-
-    st.plotly_chart(fig2)
-    # streamlit run myApp.py
